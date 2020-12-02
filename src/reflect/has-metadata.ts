dc15f475e5aa17c2f222dc02e430cada24f4e716
Halil Teyfik <hlltvfk@gmail.com>
2020-12-02 22:43:27 +0300

    fix(has-metadata.ts): missing typings breaks models
    missing decorator metadata breaks compiling schema because of invoking Reflect functions with null
    target

--- src/reflect/has-metadata.ts
+++ src/reflect/has-metadata.ts
@@ -1,1 +1,17 @@
-export const hasMetadata = Reflect.hasMetadata;
+import { Cls, Key } from '../types';
+
+export const hasMetadata = (
+  metadataKey: unknown,
+  target: object | Cls,
+  propertyKey?: Key
+): boolean => {
+  if (
+    null == target ||
+    'object' !== typeof target ||
+    'function' !== typeof target
+  ) {
+    return false;
+  }
+
+  return Reflect.hasMetadata(metadataKey, target, propertyKey);
+};
