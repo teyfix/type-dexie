b77d6d0d139a4b2c01fa3bbdcfecc30a24c819f0
Halil Teyfik <hlltvfk@gmail.com>
2020-12-02 22:06:49 +0300

    fix(type-dexie.ts): added missing imports
    added missing imports to package's entry module

--- src/type-dexie.ts
+++ src/type-dexie.ts
@@ -9,4 +9,15 @@ export * from './decorator/unique';
 export * from './util/entity-compiler';
 export * from './util/schema-compiler';
 
+export * from './error/compound-not-associated.error';
+export * from './error/empty-entity.error';
+export * from './error/insufficient-key-path.error';
+export * from './error/missing-key-path.error';
+export * from './error/missing-primary-key.error';
+export * from './error/multiple-implementations.error';
+export * from './error/must-be-camel-cased.error';
+export * from './error/not-a-constructor.error';
 export * from './error/not-an-entity.error';
+export * from './error/primary-key-already-defined.error';
+export * from './error/primary-key-not-associated.error';
+export * from './error/type-dexie.error';
